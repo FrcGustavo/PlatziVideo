48a02037b65bfc635297e57a104852a25dcc9099
FrcGustavo <hidalgo_fco520@outlook.com>
2019-08-23 18:03:05 -0500

    Curso finalizado, Añadiendo los propTypes al componente carousel item

---
+++ README.md
@@ -0,0 +1,4 @@
+# Platzi Video by Platzi
+
+Proyecto del "Curso Práctico de React JS" en platzi.
+
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Header from './../components/Header';
 import Search from '../components/Search';
@@ -7,51 +7,46 @@ import Carousel from './../components/Carousel';
 import CarouselItem from './../components/CarouselItem';
 import Footer from './../components/Footer';
 
+import useInitialState from '../hooks/useInitialState';
+
 import './../assets/styles/App.scss';
 
-const App = () => (
-    <div className="App">
-        <Header />
-        <Search />
-        <Categories title="Mi Lista">
-            <Carousel>
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-            </Carousel>
-        </Categories>
+const API = 'http://localhost:3000/initalState';
 
-        <Categories title="Tendencias">
-            <Carousel>
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-            </Carousel>
+const App = () => {
+    const initialState = useInitialState(API);
+    return initialState.length === 0 ? <h1>Loading...</h1> : (
+        <div className="App">
+            <Header />
+            <Search />
+            {initialState.mylist.length > 0 &&
+                <Categories title="Mi Lista">
+                    <Carousel>
+                        {initialState.mylist.map(item => 
+                            <CarouselItem key={item.id} {...item} />
+                        )}
+                    </Carousel>
+                </Categories>
+            }
+            
+            <Categories title="Tendencias">
+                <Carousel>
+                    {initialState.trends.map(item => 
+                        <CarouselItem key={item.id} {...item} />
+                    )}
+                </Carousel>
+            </Categories>
 
-        </Categories>
-        
-        <Categories title="Originales de PlatziVideo">
-            <Carousel>
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-            </Carousel>
-        </Categories>
-        
-        <Footer />
-    </div>
-);
+            <Categories title="Originales de PlatziVideo">
+                <Carousel>
+                    {initialState.originals.map(item => 
+                        <CarouselItem key={item.id} {...item} />
+                    )}
+                </Carousel>
+            </Categories>
+            <Footer />
+        </div>
+    );
+}
 
 export default App;
