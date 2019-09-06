1cba11d3584e0ee9cc2ea09f48ca37661fb03978
FrcGustavo <hidalgo_fco520@outlook.com>
2019-09-06 16:35:21 -0500

    Add Layout from header and footer and all sections

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,52 +1,50 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-import Header from '../components/Header';
-import Search from '../components/Search';
-import Categories from '../components/Categories';
-import Carousel from '../components/Carousel';
-import CarouselItem from '../components/CarouselItem';
-import Footer from '../components/Footer';
+import Search from "../components/Search";
+import Categories from "../components/Categories";
+import Carousel from "../components/Carousel";
+import CarouselItem from "../components/CarouselItem";
 
-import useInitialState from '../hooks/useInitialState';
+import useInitialState from "../hooks/useInitialState";
 
-import './../assets/styles/App.scss';
+import "./../assets/styles/App.scss";
 
-const API = 'http://localhost:3000/initalState';
+const API = "http://localhost:3000/initalState";
 
 const Home = () => {
-    const initialState = useInitialState(API);
-    return initialState.length === 0 ? <h1>Loading...</h1> : (
-        <div className="App">
-            <Header />
-            <Search />
-            {initialState.mylist.length > 0 &&
-                <Categories title="Mi Lista">
-                    <Carousel>
-                        {initialState.mylist.map(item => 
-                            <CarouselItem key={item.id} {...item} />
-                        )}
-                    </Carousel>
-                </Categories>
-            }
-            
-            <Categories title="Tendencias">
-                <Carousel>
-                    {initialState.trends.map(item => 
-                        <CarouselItem key={item.id} {...item} />
-                    )}
-                </Carousel>
-            </Categories>
+  const initialState = useInitialState(API);
+  return initialState.length === 0 ? (
+    <h1>Loading...</h1>
+  ) : (
+    <>
+      <Search />
+      {initialState.mylist.length > 0 && (
+        <Categories title="Mi Lista">
+          <Carousel>
+            {initialState.mylist.map(item => (
+              <CarouselItem key={item.id} {...item} />
+            ))}
+          </Carousel>
+        </Categories>
+      )}
 
-            <Categories title="Originales de PlatziVideo">
-                <Carousel>
-                    {initialState.originals.map(item => 
-                        <CarouselItem key={item.id} {...item} />
-                    )}
-                </Carousel>
-            </Categories>
-            <Footer />
-        </div>
-    );
-}
+      <Categories title="Tendencias">
+        <Carousel>
+          {initialState.trends.map(item => (
+            <CarouselItem key={item.id} {...item} />
+          ))}
+        </Carousel>
+      </Categories>
+
+      <Categories title="Originales de PlatziVideo">
+        <Carousel>
+          {initialState.originals.map(item => (
+            <CarouselItem key={item.id} {...item} />
+          ))}
+        </Carousel>
+      </Categories>
+    </>
+  );
+};
 
 export default Home;
